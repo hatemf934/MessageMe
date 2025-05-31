51436c87e264a1eddd88de4a0dd6fd4d48450146
hatemfathy <hatemf934@gmail.com>
2025-06-01 02:20:44 +0300

    edit assets file

--- lib/core/assets_manager.dart
+++ lib/core/assets_manager.dart
@@ -1,7 +1,7 @@
 class AssetsManager {
   AssetsManager._();
   static String kprofile = "assets/no-dp_16.webp";
-  static String klogo = "assets/logo-removebg-preview.png";
+  static String klogo = "assets/logo_remove.png";
   static String kfacelogo = "assets/images__1_-removebg-preview.png";
   static String kgooglelogo = "assets/images-removebg-preview.png";
 }
