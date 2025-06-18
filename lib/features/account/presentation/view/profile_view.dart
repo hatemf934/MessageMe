1a31038bbb8e35c87303221d181390b5252df197
hatemfathy <hatemf934@gmail.com>
2025-06-19 02:38:12 +0300

    finish ui profile view

--- lib/features/account/presentation/view/profile_view.dart
+++ lib/features/account/presentation/view/profile_view.dart
@@ -1,17 +1,40 @@
-import 'package:chat_group/constant.dart';
 import 'package:chat_group/core/utils/assets_manager.dart';
-import 'package:chat_group/core/utils/colorsmanager.dart';
+import 'package:chat_group/core/utils/fontsizemanager.dart';
 import 'package:chat_group/core/utils/paddingmanager.dart';
 import 'package:chat_group/core/utils/routemanger.dart';
 import 'package:chat_group/core/utils/textmanager.dart';
 import 'package:chat_group/core/utils/widthandhightmanager.dart';
+import 'package:chat_group/core/widget/showmodel.dart';
 import 'package:chat_group/features/account/presentation/view/widgets/list_tile_profile.dart';
 import 'package:chat_group/features/account/presentation/view/widgets/row_app_bar_profile.dart';
+import 'package:chat_group/features/authapp/presentation/view/widgets/profile_picture.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class Profileview extends StatelessWidget {
+class Profileview extends StatefulWidget {
   const Profileview({super.key});
   static String id = RouteManager.kProfileViewPersonal;
+
+  @override
+  State<Profileview> createState() => _ProfileviewState();
+}
+
+class _ProfileviewState extends State<Profileview> {
+  final ImagePicker _picker = ImagePicker();
+  XFile? selectedImage; // لتخزين الصورة المختارة
+
+  // دالة لفتح الخيارات (كاميرا أو معرض)
+  void _onEditPressed() async {
+    // await requestCameraPermission();
+    await showOptionsDialog(context, (source) async {
+      final pickedImage = await _picker.pickImage(source: source);
+      if (pickedImage != null) {
+        setState(() {
+          selectedImage = pickedImage; // تحديث الصورة
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,19 +46,10 @@ class Profileview extends StatelessWidget {
             SizedBox(height: Hightmanager.h20),
             const RowAppBarProfile(),
             SizedBox(height: Hightmanager.h30),
-            Container(
-              width: sizeProfilePhoto,
-              height: sizeProfilePhoto,
-              decoration: BoxDecoration(
-                color: Colorsmanager.kGrey300,
-                shape: BoxShape.circle,
-              ),
-              child: ClipRRect(
-                  borderRadius: BorderRadius.circular(sizeProfilePhoto / 2),
-                  child: Image.asset(
-                    AssetsManager.kprofile,
-                    fit: BoxFit.cover,
-                  )),
+            ProfilePictureWithEditIcon(
+              size: Fontsizemanager.font150,
+              imageAsset: selectedImage?.path ?? AssetsManager.kprofile,
+              onEditPressed: _onEditPressed,
             ),
             SizedBox(height: Hightmanager.h30),
             ListTileProfile(
@@ -54,19 +68,19 @@ class Profileview extends StatelessWidget {
               subtitle: "hatemf934@gmail.com",
             ),
             ListTileProfile(
-              iconDataLeading: Icons.email,
-              title: Textmanager.kEmail,
-              subtitle: "hatemf934@gmail.com",
+              iconDataLeading: Icons.public,
+              title: Textmanager.kCountry,
+              subtitle: "Egypt",
             ),
             ListTileProfile(
-              iconDataLeading: Icons.email,
-              title: Textmanager.kEmail,
-              subtitle: "hatemf934@gmail.com",
+              iconDataLeading: Icons.calendar_month,
+              title: Textmanager.kDateOfBrith,
+              subtitle: "25/5/2004",
             ),
             ListTileProfile(
-              iconDataLeading: Icons.email,
-              title: Textmanager.kEmail,
-              subtitle: "hatemf934@gmail.com",
+              iconDataLeading: Icons.face,
+              title: Textmanager.kGender,
+              subtitle: "male",
             ),
           ],
         ),
